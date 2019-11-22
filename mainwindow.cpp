040196364727a325b7c994fed7cdbf536a040e30
marcinkociolek <marcin.kociolek@gmail.com>
2019-11-22 19:21:50 +0100

    Almost ready

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -19,6 +19,14 @@ using namespace boost::filesystem;
 void ReadFolder(path FileFolder, std::vector<std::string> *FileNamesVector, string FilePaternStr = ".+")
 {
     FileNamesVector->clear();
+    if (!exists(FileFolder))
+    {
+        return;
+    }
+    if (!is_directory(FileFolder))
+    {
+        return;
+    }
     regex FilePattern(FilePaternStr);
     for (directory_entry& FileToProcess : directory_iterator(FileFolder))
     {
@@ -46,7 +54,88 @@ string StringVectorToString(std::vector<std::string> FileNamesVector)
 }
 
 //------------------------------------------------------------------------------------------------------------------------------
+string EvaluatePrediction(path PredictedFileToProcess)
+{
+    string OutStr = "";
+    if(!exists(PredictedFileToProcess))
+        return "file" + PredictedFileToProcess.string() + "does not exists";
 
+    //std::vector<int> PredictedIntVector;
+    //std::vector<std::string> PredictedStdVector;
+    //std::vector<std::string> TrueStdVector;
+
+    std::ifstream inFile(PredictedFileToProcess.string());
+    if (!inFile.is_open())
+    {
+        return "file" + PredictedFileToProcess.string() + "unable to open";
+    }
+    // ------------------read params from file-----------------------------
+
+    string Line;
+
+
+    int samplesCount = 0;
+    int errorCount = 0;
+    size_t position;
+    while(inFile.good())
+    {
+        samplesCount++;
+
+        getline(inFile,Line);
+
+        if(Line == "")
+            break;
+
+        position = Line.find(" ");
+        string predictedInt = Line.substr(0,position);
+        Line = Line.substr(position + 1);
+
+        position = Line.find(" : ");
+        string predictedStr = Line.substr(0,position);
+        Line = Line.substr(position + 3);
+
+        position = Line.find(" ");
+        string trueStr = Line.substr(0,position);
+
+
+        string Result;
+        if(predictedStr == trueStr)
+        {
+            Result = " OK";
+        }
+        else
+        {
+            Result = " False";
+            errorCount++;
+        }
+
+        OutStr += to_string(samplesCount)
+                  + "-" + predictedInt
+                  + "-" + predictedStr
+                  + "-" + trueStr
+                  + "-" + Result
+                  + "\n";
+
+    }
+    OutStr += "\n";
+    OutStr += "-----------------------\n";
+    OutStr += "     Summatu\n";
+    OutStr += "-----------------------\n";
+
+    OutStr += "Samples Count " + to_string(samplesCount) + "\n";
+    OutStr += "Error Count " + to_string(errorCount) + "\n";
+    if(samplesCount)
+    {
+       double errorPerc = (double)errorCount/(double)samplesCount * 100.0;
+       OutStr += "Error % " + to_string(errorPerc) + "\n";
+    }
+    else
+        OutStr += "Error % -1\n";
+
+
+    inFile.close();
+    return OutStr;
+}
 
 //------------------------------------------------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------------------------------------------
@@ -64,4 +153,97 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+//------------------------------------------------------------------------------------------------------------------------------
+void MainWindow::OpenPredictorOutputFotder()
+{
+    if (!exists(PredictorOutputFolder))
+    {
+        ui->textEditOut->append(QString::fromStdString("Predictor Output folder : " + PredictorOutputFolder.string()+ " not exists "));
+    }
+    if (!is_directory(PredictorOutputFolder))
+    {
+        ui->textEditOut->append(QString::fromStdString( "Predicto rOutput folder : " + PredictorOutputFolder.string()+ " This is not a directory path "));
+    }
+    ui->lineEditPredictorOutputFolder->setText(QString::fromStdString(PredictorOutputFolder.string()));
+    ReadFolder(PredictorOutputFolder, &PredictorOutputFileNamesVector);
+    ui->textEditPredictorOutFiles->clear();
+    ui->textEditPredictorOutFiles->append(QString::fromStdString(StringVectorToString(PredictorOutputFileNamesVector)));
+    int nrOfFiles = PredictorOutputFileNamesVector.size();
+    ui->spinBoxPredictedFileToShow->setMaximum(nrOfFiles-1);
+    ShowResult();
+
+}
+//------------------------------------------------------------------------------------------------------------------------------
+void MainWindow::OutputFotder()
+{
+    if (!exists(OutputFolder))
+    {
+        ui->textEditOut->append(QString::fromStdString("Output folder : " + OutputFolder.string()+ " not exists "));
+    }
+    if (!is_directory(OutputFolder))
+    {
+        ui->textEditOut->append(QString::fromStdString( "Output folder : " + PredictorOutputFolder.string()+ " This is not a directory path "));
+    }
+    ui->lineEditPredictorOutputFolder->setText(QString::fromStdString(PredictorOutputFolder.string()));
+
+}
+//------------------------------------------------------------------------------------------------------------------------------
+void MainWindow::ShowResult()
+{
+
+    if(!PredictorOutputFileNamesVector.size())
+        return;
+    unsigned long long fileNrToProcess = ui->spinBoxPredictedFileToShow->value();
+    path PredictorOutputFile = PredictorOutputFolder;
+    PredictorOutputFile.append(PredictorOutputFileNamesVector[fileNrToProcess]);
+    string OutText =  EvaluatePrediction(PredictorOutputFile);
+    ui->textEditOutFileContent->clear();
+    ui->textEditOutFileContent->setText(QString::fromStdString(OutText));
+
+}
+//------------------------------------------------------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------------------------------------------------------
+
+
+//------------------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------
+//          Slots
+//------------------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------
+
+void MainWindow::on_pushButtonOpenPredictorOutputFolder_clicked()
+{
+    QFileDialog dialog(this, "Open Folder");
+    dialog.setFileMode(QFileDialog::Directory);
+    dialog.setDirectory(QString::fromStdString(PredictorOutputFolder.string()));
+
+    if(dialog.exec())
+    {
+        PredictorOutputFolder = dialog.directory().path().toStdWString();
+    }
+    else
+        return;
+    OpenPredictorOutputFotder();
+}
+
+void MainWindow::on_pushButtonOpenOutFolder_clicked()
+{
+    QFileDialog dialog(this, "Open Folder");
+    dialog.setFileMode(QFileDialog::Directory);
+    dialog.setDirectory(QString::fromStdString(OutputFolder.string()));
+
+    if(dialog.exec())
+    {
+        OutputFolder = dialog.directory().path().toStdWString();
+    }
+    else
+        return;
+    OutputFotder();
+}
+
+void MainWindow::on_spinBoxPredictedFileToShow_valueChanged(int arg1)
+{
+    ShowResult();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,26 +15,33 @@ class MainWindow : public QMainWindow
 
 public:
 
-    boost::filesystem::path QMaZdaFolder;
-    boost::filesystem::path ImageFolder;
-    boost::filesystem::path ROIFolder;
-    boost::filesystem::path OptionsFolder;
     boost::filesystem::path MzFeaturesOutFolder;
-    boost::filesystem::path ClassyfiersFolder;
-    boost::filesystem::path ClassyfiersOptionFile;
     boost::filesystem::path PredictorOutputFolder;
-    boost::filesystem::path BatFolder;
+    boost::filesystem::path OutputFolder;
 
-    std::string OutFilePrefix;
-    std::string BatFileName;
-    std::string FilePattern;
+    std::vector<std::string> PredictorOutputFileNamesVector;
 
-    std::vector<std::string> ImageFileNamesVector;
-    std::vector<std::string> RoiFileNamesVector;
-    std::vector<std::string> OptionsFileNamesVector;
+    std::vector<int> PredictedClassIntVector;
+    std::vector<std::string> PredictedClassStrVector;
+    std::vector<std::string> TrueClassStrVector;
+
+    int samplesCount;
+    int ErrorCount;
 
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+    void OpenPredictorOutputFotder();
+    void OutputFotder();
+    void ShowResult();
+    //string EvaluatePrediction(int fileNrToProcess,std::vector<std::string> PredictorOutputFileNamesVector);
+
+private slots:
+    void on_pushButtonOpenPredictorOutputFolder_clicked();
+
+    void on_pushButtonOpenOutFolder_clicked();
+
+    void on_spinBoxPredictedFileToShow_valueChanged(int arg1);
 
 private:
     Ui::MainWindow *ui;
